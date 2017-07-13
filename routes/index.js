382eae3b7191f09fee87c20ca120316fe2bade53
mwqdev <quarles.michael@gmail.com>
2017-07-13 10:57:03 -0400

    File structure changes, middlewares
    Added routes directory
    Added index routes file
    Added body-parser
    Moved data to lib/data.js
    Added error handling middleware
    Added scripts to package.json

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+
+// TODO update routes
+router.get('/', function(request, response){
+    response.render("index");
+});
+
+router.get('/paragraph/:amount', function(request, response){
+    let lebowski_array = shuffle_array(lebowski);
+    let model = populate_ipsum((request.params.amount*5)-1, lebowski_array);
+    response.render("index", {model: model});
+});
+
+router.get('/sentence/:amount', function(request, response){
+    let lebowski_array = shuffle_array(lebowski);
+    let model = populate_ipsum(request.params.amount-1, lebowski_array);
+    response.render("index", {model: model});
+});
+
+router.get('/word/:amount', function(request, response){
+    let lebowski_array = shuffle_array(lebowski.split(' '));
+    let model = lebowski_array.splice(0, request.params.amount).join(' ');
+    response.render("index", {model: model});
+});
+
+module.exports = router;
