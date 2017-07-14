cc3692f85e425cebf534973e3d0eda97f7a17ebf
AHarryHughes <Ahhughes8@gmail.com>
2017-07-14 19:13:55 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-
 const lebowski_data = require('../lib/data');
 
 // TODO update routes
@@ -11,52 +10,48 @@ router.get('/', (request, response) => {
     let sentences = request.query.sentences;
     let paragraphs = request.query.paragraphs;
 
-    let model = lebowski_ipsum(rating, sentences, paragraphs);
+    let model = {};
+    model.paragraph = lebowski_ipsum(rating, 'paragraph', paragraphs);
+    model.sentence = lebowski_ipsum(rating, 'sentence', sentences);
 
     return response.render('index', model);
   }
   return response.render('index');
 });
 
+
 // quote arrays
 let lebowski = lebowski_data.lebowski;
 let the_dude = lebowski_data.r_rated;
 
-function populate_ipsum(sentences, lebowski) {
-  if (!sentences) {
-    return lebowski[0];
-  }
-  return populate_ipsum(sentences - 1, lebowski) + ' ' + lebowski[sentences];
+//general functions
+function populate_ipsum(amount, pi_lebowski) {
+    if(!amount){return pi_lebowski[0];}
+    return populate_ipsum(amount-1, pi_lebowski) + ' ' + pi_lebowski[amount];
 };
 
-function shuffle_array(lebowski) {
-  let lebowski_array = [];
-
-  for (var i in lebowski) {
-    Math.round(Math.random()) ?
-      lebowski_array.push(lebowski[i]) :
-      lebowski_array.unshift(lebowski[i]);
-  }
-
-  return lebowski_array;
+function shuffle_array(sa_lebowski){
+    let lebowski_array = [];
+    for(var i = 0; i < sa_lebowski.length; i++){
+      let rand = Math.floor(Math.random() * lebowski_array.length);
+      if(rand == 0){lebowski_array.unshift(sa_lebowski[i]);}
+      else if(rand == lebowski_array.length-1){lebowski_array.push(sa_lebowski[i]);}
+      else{lebowski_array.splice(rand, 0, sa_lebowski[i]);}
+    }
+    console.log(lebowski_array[0]);
+    return lebowski_array;
 }
 
-function lebowski_ipsum(rating, sentences, paragraphs) {
-  let tmp_lebowski = lebowski;
-  let model = {};
-  model.ipsums = [];
 
-  // array creation/randomization
-  if (rating == 'r') {
-    tmp_lebowski = tmp_lebowski.concat(the_dude);
-  }
-  let lebowski_array = shuffle_array(tmp_lebowski);
-
-  for (let i = 0; i < paragraphs; i++) {
-    lebowski_array = shuffle_array(tmp_lebowski);
-    model.ipsums.push(populate_ipsum((sentences - 1), lebowski_array));
-  }
-  return model;
+function lebowski_ipsum(rating, type, amount){
+    let tmp_lebowski = lebowski;
+    if(rating == 'r'){tmp_lebowski = tmp_lebowski.concat(the_dude);}
+    let final_lebowski = shuffle_array(tmp_lebowski);
+    if(type == 'paragraph'){amount *= 5;}
+    let model = populate_ipsum((amount-1), final_lebowski);
+    return model;
 }
+
 
 module.exports = router;
+
